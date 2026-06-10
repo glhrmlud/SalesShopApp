207c0b705298cbc7b27fef7d6830c07c25dc10aa
Guilherme E. <dmgeliude@gmail.com>
2026-06-10 08:05:42 -0300

    feat: products list view into sales_panel and initial style products card

--- sales_panel/static/sales_panel/js/view.js
+++ sales_panel/static/sales_panel/js/view.js
@@ -1,19 +1,24 @@
-list_products = document.querySelector('.list_products')
+document.addEventListener("DOMContentLoaded", () => {
+  list_products = document.querySelector('.list-products')
 
-fetch('/api/products/')
-  .then(response => response.json())
+  fetch('/api/products/')
+    .then(response => response.json())
 
-  .then(data => {
-    data.products.forEach(product => {
-      const card = document.createElement('div');
-      
-      card.innerHTML = `
-      <img src='${product.image}'>
-      <h2>${product.name}</h2>
-      <p>${product.description}</p>
-      <p>R$ ${product.price}</p>
-      <p>Estoque: ${product.stock}</p>
-      `
-    list_products.appendChild(card);
-    }); 
-  });
+    .then(data => {
+      data.products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+        productCard.innerHTML = `
+        <img class='product-image' src='${product.image}'>
+        <h2 class='product-name text'>${product.name}</h2>
+        <p class='product-desc text'>${product.description}</p>
+        <p class='product-stock text'> ${product.stock}</p>
+        <div class='price-add'>
+          <p class='product-price text'> R$ ${product.price}</p>
+          <div class='circle add-btn'> {% include 'sales_panel/svg_layout/edit.svg' %} </div>
+        </div>
+        `
+      list_products.appendChild(productCard);
+      }); 
+    });
+});

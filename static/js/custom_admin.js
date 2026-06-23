756f2d810020954b79c7692c530581dd761ca190
Guilherme E. <dmgeliude@gmail.com>
2026-06-23 20:25:51 -0300

    feat: finish admnin panel style and reconfigurate tables rotes

---
+++ static/js/custom_admin.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  const changelistForm = document.querySelector('#changelist-form');
+  const actions = document.querySelector('.actions');
+  const changelistFilter = document.querySelector('#changelist-filter');
+  changelistFilter.classList += ' square border';
+  changelistForm.classList += ' square';
+})
